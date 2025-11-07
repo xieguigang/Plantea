839c8f85979ea2c928445cdb43e814b33453f24a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-07 16:20:25 +0800

    make improvements of the data model

---
+++ src/Plantea/RegulationFootprint.vb
@@ -0,0 +1,7 @@
+﻿Imports SMRUCC.genomics.Model.Network.VirtualFootprint.DocumentFormat
+
+Public Class RegulationFootprint : Inherits RegulatesFootprints
+
+    Public Property chromosome As String
+
+End Class
