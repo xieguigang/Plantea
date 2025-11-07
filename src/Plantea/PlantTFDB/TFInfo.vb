4aaad63d6e4ab736f530ef921ea5fb628b7f2d9f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-07 22:01:29 +0800

    fix of the gene id

--- src/Plantea/PlantTFDB/TFInfo.vb
+++ src/Plantea/PlantTFDB/TFInfo.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+﻿Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Serialization.JSON
 
 Public Class TFInfo
@@ -10,7 +11,11 @@ Public Class TFInfo
 
     Public ReadOnly Property gene_id As String
         Get
-            Return protein_id
+            Dim prot_id As String = protein_id
+
+            prot_id = RemoveTrailingIntegerSuffix(prot_id)
+
+            Return prot_id
         End Get
     End Property
 
@@ -29,6 +34,15 @@ Public Class TFInfo
 
     Public Overrides Function ToString() As String
         Return Me.GetJson
+    End Function
+
+    Public Shared Function RemoveTrailingIntegerSuffix(inputString As String) As String
+        ' 正则表达式模式：匹配以点开头后接一个或多个数字，并且位于字符串末尾的部分
+        Dim pattern As String = "\.\d+$"
+        ' 使用 Regex.Replace 方法将匹配到的部分替换为空字符串
+        Dim result As String = Regex.Replace(inputString, pattern, String.Empty)
+
+        Return result
     End Function
 
 End Class
