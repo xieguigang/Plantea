31f3394946c31097ea4de070ec890f8b3cb41e3e
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-07 14:48:35 +0800

    run pacjafe msbuild

---
+++ @export/Plantea.d.ts
@@ -0,0 +1,8 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "Plantea" from "Plantea";
+//
+// ref=Plantea.Exports@Plantea, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+*/
