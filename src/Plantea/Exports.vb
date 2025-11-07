229b479c46ccca631b93f8f40d18491e0bf3fdc3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-07 14:45:29 +0800

    add project source files

---
+++ src/Plantea/Exports.vb
@@ -0,0 +1,8 @@
+﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("Plantea")>
+<RTypeExport("motif_link", GetType(MotifLink))>
+Module Exports
+
+End Module
---
+++ src/Plantea/PlantTFDB/MotifLink.vb
@@ -0,0 +1,14 @@
+﻿Public Class MotifLink
+
+    Public Property Matrix_id As String
+    Public Property Gene_id As String
+    Public Property Species As String
+    Public Property Method As String
+    Public Property Datasource As String
+    Public Property Datasource_ID As String
+
+    Public Overrides Function ToString() As String
+        Return Matrix_id
+    End Function
+
+End Class
