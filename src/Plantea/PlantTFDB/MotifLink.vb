5287d11135fd0818425f8537a088eff56f3de73c
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-07 17:04:39 +0800

    make improvements of the id set model

--- src/Plantea/PlantTFDB/MotifLink.vb
+++ src/Plantea/PlantTFDB/MotifLink.vb
@@ -1,7 +1,16 @@
-﻿Public Class MotifLink
+﻿Imports Microsoft.VisualBasic.Data.Framework.StorageProvider.Reflection
+
+Public Class MotifLink
 
     Public Property Matrix_id As String
-    Public Property Gene_id As String
+
+    ''' <summary>
+    ''' A set of the gene id that associated with current motif object
+    ''' </summary>
+    ''' <returns></returns>
+    <Collection("Gene_id", ", ")>
+    Public Property Gene_id As String()
+
     Public Property Species As String
     Public Property Method As String
     Public Property Datasource As String
