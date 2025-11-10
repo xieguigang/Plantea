a982b10970dfa46b5628305cb322a2c8d440643b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-11 00:59:34 +0800

    make assignment of the gene clsuter infor

--- src/Plantea/RegulationFootprint.vb
+++ src/Plantea/RegulationFootprint.vb
@@ -1,4 +1,5 @@
-﻿Imports SMRUCC.genomics.Model.Network.VirtualFootprint.DocumentFormat
+﻿Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.genomics.Model.Network.VirtualFootprint.DocumentFormat
 
 ''' <summary>
 ''' The transcription regulation network edge
@@ -10,5 +11,28 @@ Public Class RegulationFootprint : Inherits RegulatesFootprints
 
     Public Property regulator_group As String
     Public Property target_group As String
+
+    Public Shared Iterator Function AssignClassData(regs As IEnumerable(Of RegulationFootprint), kb As Dictionary(Of String, ClassClusterData)) As IEnumerable(Of RegulationFootprint)
+        For Each link As RegulationFootprint In regs.SafeQuery
+            Dim term As String = If(link.target_group, "")
+
+            If kb.ContainsKey(term) Then
+                With kb(term)
+                    link.class = .class
+                    link.subclass = .subclass
+                End With
+            End If
+
+            Yield link
+        Next
+    End Function
+
+End Class
+
+Public Class ClassClusterData
+
+    Public Property gene As String
+    Public Property [class] As String
+    Public Property subclass As String
 
 End Class
