30ac3af51613307cafb0469505a640fca13682d3
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-14 03:08:56 +0800

    create the background model

--- src/Plantea/RegulationFootprint.vb
+++ src/Plantea/RegulationFootprint.vb
@@ -1,4 +1,6 @@
 ﻿Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Text.Xml.Models
+Imports SMRUCC.genomics.Analysis.HTS.GSEA
 Imports SMRUCC.genomics.Model.Network.VirtualFootprint.DocumentFormat
 
 ''' <summary>
@@ -34,5 +36,37 @@ Public Class ClassClusterData
     Public Property gene As String
     Public Property [class] As String
     Public Property subclass As String
+
+    Public Overrides Function ToString() As String
+        Return $"[{[class]}] {gene}"
+    End Function
+
+    Public Shared Function BuildBackground(geneSet As IEnumerable(Of ClassClusterData)) As Background
+        Return New Background With {
+            .clusters = geneSet.SafeQuery _
+                .GroupBy(Function(gene) gene.class) _
+                .Select(Function(cl) CreateCluster(cl.Key, cl)) _
+                .ToArray,
+            .size = .clusters.BackgroundSize
+        }
+    End Function
+
+    Private Shared Function CreateCluster(label As String, list As IEnumerable(Of ClassClusterData)) As Cluster
+        Return New Cluster With {
+            .ID = label,
+            .names = .ID,
+            .members = CreateGeneObjects(list).ToArray
+        }
+    End Function
+
+    Private Shared Iterator Function CreateGeneObjects(list As IEnumerable(Of ClassClusterData)) As IEnumerable(Of BackgroundGene)
+        For Each gene As ClassClusterData In list
+            Yield New BackgroundGene With {
+                .accessionID = gene.gene,
+                .name = .accessionID,
+                .locus_tag = New NamedValue(.accessionID)
+            }
+        Next
+    End Function
 
 End Class
