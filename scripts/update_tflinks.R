2224848dc57b71c28dee024b4c4ea470df63701f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-13 15:04:19 +0800

    download motif links data

--- scripts/update_tflinks.R
+++ scripts/update_tflinks.R
@@ -9,7 +9,21 @@ setwd(relative_work());
 # writeLines(page, con = "download.html");
 
 let tables = Html::tables(page, filter = TRUE, plain_text = FALSE);
+let bind_motif = tables$bind_motif;
 
-for(let name in names(tables)) {
-    print(tables[[name]]);
+print(bind_motif, max.print = 13);
+
+# for(let name in names(tables)) {
+#     print(name);
+#     print(tables[[name]]);
+# }
+
+for(let bind_data in tqdm(bind_motif$"Motif information")) {
+    let url = `https://planttfdb.gao-lab.org/${Html::link(bind_data)}`;
+    let infodata = toString(requests.get(url));
+    let name = basename(url) |> gsub("_TF_binding_motifs_information");
+
+    writeLines(infodata, con = 
+        relative_work(`../data/PlantTFDB/bind_motif/${name}.txt`));
+    sleep(1);
 }
