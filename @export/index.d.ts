34ffac6dab4f62db3f0ea1d5717684f32fe24715
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-22 00:23:34 +0800

    run package msbuild

--- @export/index.d.ts
+++ @export/index.d.ts
@@ -14,7 +14,15 @@ declare namespace Plantea {
    function load_motif_links(data_frame?: any): object;
    /**
    */
+   function locate_meme_dir(code: any): object;
+   /**
+   */
    function open_meme(code: any): object;
+   /**
+     * @param outdir default value Is ``./``.
+     * @param n_threads default value Is ``8``.
+   */
+   function plant_motif_search(seqfile: any, plant_code: any, outdir?: any, n_threads?: any): object;
    /**
    */
    function PlantTFDB_motifs(): object;
