ee5906ad477fcd39dbf2323a0d5876f1db17525b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-22 00:10:57 +0800

    database api test success

--- test/test_meme.R
+++ test/test_meme.R
@@ -3,3 +3,9 @@ require(Plantea);
 let db = open_meme("Tae");
 
 print([db]::FamilyList);
+
+let pwm = db |> load_motifs(name = "TRAES3BF001100080CFD_g");
+
+for(let motif in pwm) {
+    print(as.data.frame(motif));
+}
