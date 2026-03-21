c7d7f09f910759364e0e04205a233fdf27d2d6aa
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-21 23:05:11 +0800

    Update motif_db.R

--- R/PlantTFDB/motif_db.R
+++ R/PlantTFDB/motif_db.R
@@ -1,7 +1,16 @@
+imports "motif_tool" from "TRNtoolkit";
+
 const PlantTFDB_motifs = function() {
     system.file("data/PlantTFDB/PlantTFDB_TF_binding_motifs_from_experiments.xml",
         package = "Plantea"
     )
     |> Plantea::load_motifdb()
     ;
+}
+
+const open_meme = function(code) {
+    let data_dir = file.path(@datadir, code);
+    let meme_dir = file.path(data_dir, `${code}_TF_binding_motifs_individual`);
+
+    motif_tool::open_meme_dir(meme_dir);
 }
