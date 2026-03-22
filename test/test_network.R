07a43fccc720232c1cdb95e0348c18d83a71cc09
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-22 22:18:31 +0800

    Update test_network.R

--- test/test_network.R
+++ test/test_network.R
@@ -1,5 +1,7 @@
-require();
+require(Plantea);
 
 imports "bioseq.patterns" from "seqtoolkit";
 
-read.scans
+let sites = read.scans("F:\datapool\20260312\20260322\Lanmai_upstream_500bp_vs_PlantTFDB_TF_binding_motifs_from_experiments_motif_scan.csv");
+
+print(sites);
