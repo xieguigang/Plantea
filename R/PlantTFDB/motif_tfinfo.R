924aefb814d231af80403566a58cca4f5c916048
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-03-23 16:22:40 +0800

    Update motif_tfinfo.R

--- R/PlantTFDB/motif_tfinfo.R
+++ R/PlantTFDB/motif_tfinfo.R
@@ -1,9 +1,17 @@
 imports "bioseq.fasta" from "seqtoolkit";
 
 const motif_tfinfo = function(code) {
+    extract_tf_info(motif_tfseq(code));
+}
+
+#' Load internal TF sequence dataset
+#' 
+#' @return a fasta sequence collection.
+#' 
+const motif_tfseq = function(code) {
     let data_dir = file.path(@datadir, code);
     let seqfile = file.path(data_dir,`${code}_pep.fas`);
     let tf_seqs = read.fasta(seqfile);
 
-    extract_tf_info(tf_seqs);
+    return(tf_seqs);
 }
